dcfb23ded9595c011cf70a7ebe27e90e861593bd
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-10-27 09:18:27 +0000

    HeaderContainer

---
+++ src/containers/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
+
+export function HeaderContainer({ children }) {
+    return (
+        <Header>
+            <Header.Frame>
+                <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+                <Header.ButtonLink to={ROUTES.SING_IN}>
+                    Sign In
+                </Header.ButtonLink>
+            </Header.Frame>
+            {children}
+        </Header>
+    );
+}
