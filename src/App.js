979aa5424e46a234b119ec350c60cabe805a6396
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-10-30 20:50:05 +0000

    Saving alterations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home } from './pages';
+import * as ROUTES from './constants/routes';
+import {
+    IsUserRedirect,
+    isUserRedirect,
+    ProtectedRoute,
+} from './helpers/routes';
 
 function App() {
+    const user = null;
+
     return (
         <Router>
             <Switch>
-                <Home />
+                <IsUserRedirect
+                    user={user}
+                    loggedInPath={ROUTES.BROWSE}
+                    path={ROUTES.HOME}
+                >
+                    <Home />;
+                </IsUserRedirect>
             </Switch>
         </Router>
     );
