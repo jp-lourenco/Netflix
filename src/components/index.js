8b1f6319431b555b25b9e27cc7aafa1531276573
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-02 11:41:54 +0000

    export new components

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,7 @@ export { default as Header } from './header';
 export { default as Jumbotron } from './jumbotron';
 export { default as Accordion } from './accordion';
 export { default as Footer } from './footer';
+export { default as Card } from './card';
+export { default as Form } from './form';
+export { default as Loading } from './loading';
+export { default as Player } from './player';
